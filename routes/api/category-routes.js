90b66c2ad02e4fda4f7a7e227fbbd7591c538123
Stephen Yacas <syacas123@gmail.com>
2021-05-19 19:56:59 -0400

    modified category, product, productTag, and tag model files

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,17 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+
+  try { 
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  console.log(categoryData)
 });
 
 router.get('/:id', (req, res) => {
